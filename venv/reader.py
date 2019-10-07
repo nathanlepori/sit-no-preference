3e385db8531cd252aec8e9d5972645ebd93a13e3
kwanz <kwanzhengyi@gmail.com>
2019-10-07 15:10:52 +0800

    testing for update to github

--- venv/reader.py
+++ venv/reader.py
@@ -20,26 +20,9 @@ import Functions1
 #     for line in ins:
 #         array.append(line)
 
-def readFiltered(filename):
-    wholeData = [];
-    with open(filename, 'r') as reader:
-        for line in reader:
-            # for field in line: #for individual char
-            print("======")
-            # print(line)
-            tokens = nltk.word_tokenize(line)
-            # print(tokens)
-
-            # strip stop words
-            filtered_sentence = [w for w in tokens if not w in stop_words]
-            # print(filtered_sentence)
-            # wholeData.append(filtered_sentence)
-            # wholeData.extend(filtered_sentence)
-
-            wordFrequency.dfCounter(filtered_sentence, '09/06/2019', '12/06/2019')
-
-    # return wholeData
-    # return filtered_sentence
+def tokenizeColumn(df,columnName):
+    df[columnName] = df[columnName].apply(nltk.word_tokenize)
+    return df
 
 def readDate(filename,startDate,endDate):
     read = pd.read_csv(filename)
@@ -47,43 +30,19 @@ def readDate(filename,startDate,endDate):
     df = Functions1.functionSortByDateRange(df,'date',startDate,endDate)
     return df
 
-def filterStopWord(df):
+def filterStopWordColumn(df,columnName):
     stop_words = set(stopwords.words('english'))
-    # for line in df:
-    # for line in df:
-    # tokens = df.apply(lambda row: nltk.word_tokenize(row['word']), axis=1)
-    df['word'] = df['word'].apply(nltk.word_tokenize)
-    # print(df.iloc[0]['word'])
     i=0
     for row in df['word']:
-        print(df.iloc[i]['word'])
-        df.iloc[i]['word'] = [w for w in df.iloc[i]['word'] if not w in stop_words]
+        # print(df.iloc[i]['word'])
+        df.iloc[i][columnName] = [w for w in df.iloc[i][columnName] if not w in stop_words] #accessing row and remove stop words
         i+=1
-    # filtered_df = [w for w in tokens['word'] if not w in stop_words]
-    # df['word'] = [w for w in df['word'] if not w in stop_words]
-    # for row in df['word']:
-    #     print("rowVV")
-    #     print(row)
-    #     print("row^^")
-    # filtered = [w for w in df['word'][0] if not w in stop_words]
-    # print(filtered)
-        # print("filtered="+str(filtered))
-        # j = 0
-        # for word in row:
-        #     print("i=" + str(i) + " V|V j=" + str(j))
-        #     print(df['word'][i][j])
-            # if word in stop_words:
-                # df['word'][i][j]
-                # df['word'] = df['word'].replace(word,'')
-                #     print(word)
-            # j+=1
-        # if(j>1):
-        # i+=1
     return df
 
 # run this reader
 filename = '..\words.csv'
 df = readDate(filename,"10/06/2019","11/06/2019") #restrict to only between dates
-# filtered = readFiltered(filename) #filter out stop words
-df = filterStopWord(df)
+columnName = 'word'
+df = tokenizeColumn(df,columnName)
+df = filterStopWordColumn(df,columnName)
 print(df)
