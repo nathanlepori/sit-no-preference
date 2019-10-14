cb61d7fabad5ef3529280437d7e175b228b4700a
breakbrick <guobao03@gmail.com>
2019-10-14 19:59:40 +0800

    twitter

--- src/twitter/getTwitterInfo.py
+++ src/twitter/getTwitterInfo.py
@@ -1,44 +1,33 @@
-import csv
-
+import unicodecsv as csv
 import tweepy
 
-# defining token keys
+# Defining token keys
 access_token = '1174467219629076480-KtAYQaIyf8xI6XiBcNOwdnDxgVIxJZ'
 access_token_secret = 'NR4Gh8LdGyLznEQ1Q4D5rXaez4iApUVAJixojxFkC1fNH'
 consumer_key = 'j6HqL176aZmPCdcOEzoQgFkOy'
 consumer_secret = 'tJ20feJRMfD1S7DakSU6FzUKQC57urOZOw2P29ojGiTTTeUitX'
 
-# Function to extract tweets
-def get_tweets(username):
-    # Authorization to consumer key and consumer secret
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# OAuth process, using the keys and tokens
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 
-    # Access to user's access key and access secret
-    auth.set_access_token(access_token, access_token_secret)
+# Creation of the actual interface, using authentication
+api = tweepy.API(auth)
 
-    # Calling api
-    api = tweepy.API(auth)
+# Defining file for storing information
+output_file_path = 'tweets_information.csv'
 
-    # 200 tweets to be extracted
-    number_of_tweets = 200
-    tweets = api.user_timeline(screen_name=username)
+with open(output_file_path, 'wb') as output_file:
+    csv_writer = csv.writer(output_file, quoting=csv.QUOTE_ALL)
+    headers = ['Timestamp', 'Tweets', 'Status']
+    csv_writer.writerow(headers)
 
-    # Empty Array
-    tmp = []
-
-    # create array of tweet information: username,
-    # tweet id, date/time, text
-    tweets_for_csv = [tweet.text for tweet in tweets]  # CSV file created
-    for j in tweets_for_csv:
-        # Appending tweets to the empty array tmp
-        tmp.append(j)
-
-        # Printing the tweets
-    print(tmp)
-
-
-# Driver code
-if __name__ == '__main__':
-    # Here goes the twitter handle for the user
-    # whose tweets are to be extracted.
-    get_tweets("amazonIN")
+    for tweet in tweepy.Cursor(api.user_timeline, screen_name='realDonaldTrump', tweet_mode='extended').items():
+        # Check if the tweet is a retweet or tweet made by the user
+        if tweet.full_text.startswith("RT @"):
+            csv_writer.writerow([tweet.created_at, tweet.retweeted_status.full_text.encode('UTF-8-sig'), 'RT'])
+            # print tweet.created_at, tweet.retweeted_status.full_text.encode('UTF-8-sig'), 'RT'
+        else: # Tweet made by user without 'RT @'
+            csv_writer.writerow([tweet.created_at, tweet.full_text.encode('UTF-8-sig'), 'Tweet'])
+            # print tweet.created_at, tweet.full_text.encode('UTF-8-sig')
+csv_writer.close()
