de78595a82a371ebf5147ceb8b716c5ae387ecb0
breakbrick <guobao03@gmail.com>
2019-10-14 14:06:43 +0800

    browser_history

--- src/browser_history/getBrowserHistory.py
+++ src/browser_history/getBrowserHistory.py
@@ -10,41 +10,21 @@ def getChromeHistoryFolder(): # getting the path location of chrome history file
     history_db = os.path.join(data_path, 'history')
     return history_db
 
-# need to take a look at this link:
-# https://stackoverflow.com/questions/43435216/sql-after-the-join-remove-duplicate-rows
-
 with sq.connect(getChromeHistoryFolder()) as conn:
     conn.text_factory = str
     c = conn.cursor()
     output_file_path = 'chrome_history.csv'
     with open(output_file_path, 'wb') as output_file:
         csv_writer = csv.writer(output_file, quoting=csv.QUOTE_ALL)
-        headers = ['URL', 'Title', 'Visit Count', 'Last Accessed Date-Time (GMT)',
-                   'Duration of Last Accessed (in seconds)']
+        headers = ['URL', 'Title', 'Visit Count', 'Last Accessed Date-Time (GMT+8)']
         csv_writer.writerow(headers)
-        # epoch = datetime(1601, 1, 1)
         for row in (c.execute(
-                'select u.url, u.title, u.visit_count, '
-                'datetime((u.last_visit_time/1000000)-11644473600,\'unixepoch\', \'localtime\'), '
-                '(v.visit_duration/1000000) '
-                'from urls as u '
-                'inner join visits as v on u.id = v.id '
-                # 'group by u.url, u.title, u.visit_count, u.last_visit_time, v.visit_duration '
-                'order by u.last_visit_time desc;')):
+                'SELECT urls.url, urls.title, urls.visit_count, '
+                'datetime((urls.last_visit_time/1000000)-11644473600,\'unixepoch\', \'localtime\') '
+                'FROM urls '
+                'order by urls.last_visit_time desc;')):
             row = list(row)
-            # url_time = epoch + timedelta(microseconds=row[3])
-            # row[3] = url_time
             csv_writer.writerow(row)
-        # for row in (c.execute(
-        #         'SELECT urls.url, urls.title, urls.visit_count, '
-        #         'datetime((urls.last_visit_time/1000000)-11644473600,\'unixepoch\', \'localtime\') '
-        #         'FROM urls '
-        #         'order by urls.last_visit_time desc;')):
-        #     row = list(row)
-        #     # url_time = epoch + timedelta(microseconds=row[3])
-        #     # row[3] = url_time
-        #     csv_writer.writerow(row)
-os.startfile(output_file_path)
 
 def getMozillaHistoryFile(): # getting the path location of firefox history file
     # the following 5 lines of code will retrieve the profile of firefox
@@ -62,12 +42,12 @@ with sq.connect(getMozillaHistoryFile()) as conn:
     output_file_path = 'firefox_history.csv'
     with open(output_file_path, 'wb') as output_file:
         csv_writer = csv.writer(output_file, quoting=csv.QUOTE_ALL)
-        headers = ['URL', 'Title', 'Visit Count', 'Last Accessed (GMT)']
+        headers = ['URL', 'Title', 'Visit Count', 'Last Accessed DAte-Time (GMT+8)']
         csv_writer.writerow(headers)
         for row in (c.execute(
                 'select moz_places.url, moz_places.title, moz_places.visit_count, '
                 'datetime(moz_places.last_visit_date/1000000,\'unixepoch\', \'localtime\') '
-                'from moz_places order by last_visit_date desc')):
+                'from moz_places '
+                'order by last_visit_date desc')):
             row = list(row)
             csv_writer.writerow(row)
-os.startfile(output_file_path)
