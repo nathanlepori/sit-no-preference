54e9b20c492259b0f962b73ad29c83fffbcabed5
Nathan Lepori <nathan.lepori@hotmail.com>
2019-11-09 13:24:06 +0800

    Fixed linux install script

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Move to script directory
-cd $(dirname "$0")
+cd "$(dirname "$0")" || exit
 
 # Create virtual environment using Python 3
 python3 -m venv ./venv
 
 # Install requirements from venv
-./venv/Scripts/pip install -r requirements.txt
+./venv/bin/pip install -r requirements.txt
